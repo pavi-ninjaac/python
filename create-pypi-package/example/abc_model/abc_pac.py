6e619a9c5b7d8082a0634e7e7242956337ffc14f
PaviNinjac <51699297+pavi-ninjaac@users.noreply.github.com>
2021-08-22 15:58:26 +0530

    Add files via upload

---
+++ create-pypi-package/example/abc_model/abc_pac.py
@@ -0,0 +1,8 @@
+import math
+
+class add:
+    def __init__(self) -> None:
+        pass
+    def addition(self,a ,b):
+        print(a+b)
+        
---
+++ create-pypi-package/example/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='abc_model',
+    version="0.1",
+    description="First demo pack to uplod in pypi",
+    packages=["abc_model"],
+    author="pavi",
+    author_email="pavi@gmail.com",
+    zip_safe=False
+
+)
